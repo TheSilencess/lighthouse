6f7d863ec4bde90f1dc837b00f0ae04fce270bc7
jordonez-2025428 <jordonez-2025428@kinal.edu.gt>
2026-06-19 15:13:23 -0600

    feat: creación de los scripts de interactividad para la pagina web

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Manejo del menú desplegable para dispositivos móviles
+const mobileMenu = document.getElementById('mobile-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+mobileMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    mobileMenu.classList.toggle('is-active');
+});
+
+// Cerrar menú al hacer click en cualquier link de navegación
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
+});
+
+// Cambiar estado activo en el Header según el Scroll del usuario
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= (sectionTop - 150)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
